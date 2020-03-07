87cfaeb0dca476dcd3ba272f391df7135098a66e
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 20:08:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -20,37 +20,37 @@ class Owner
 
 
 # class Owner
-# 
+#
 #   attr_reader :species, :owner, :name
 #   attr_accessor :pets,
 #   #Instance Methods
-# 
+#
 #   @@all = []
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def self.reset_all
 #     @@all.clear
 #   end
-# 
+#
 #   def initialize(name, species="human")
 #     @name = name
 #     @species = species
 #     @owner << self
 #   end
-# 
+#
 #   def say_species
 #     "I am a human."
 #   end
-# 
-# 
-# 
+#
+#
+#
 #   def name
 #     @name
 #   end
-# 
-# 
-# 
+#
+#
+#
 # end
