a1ffccc552b7545c41bfdac43779658b3583d073
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 20:08:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,35 +1,56 @@
 class Owner
+  @@all= []
+  attr_accessor :name, :pets
+  attr_reader :species
 
-  attr_reader :species, :owner, :name
-  attr_accessor :pets,
-  #Instance Methods
+  # Instance Methods #
 
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def self.reset_all
-    @@all.clear
-  end
-
-  def initialize(name, species="human")
-    @name = name
+  def initialize(species)
     @species = species
-    @owner << self
+    @@all << self
+    @pets = { :dogs => [], :cats => []}
   end
 
   def say_species
-    "I am a human."
+    return "I am a #{@species}."
   end
 
 
 
-  def name
-    @name
-  end
 
 
-
-end
+# class Owner
+# 
+#   attr_reader :species, :owner, :name
+#   attr_accessor :pets,
+#   #Instance Methods
+# 
+#   @@all = []
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.reset_all
+#     @@all.clear
+#   end
+# 
+#   def initialize(name, species="human")
+#     @name = name
+#     @species = species
+#     @owner << self
+#   end
+# 
+#   def say_species
+#     "I am a human."
+#   end
+# 
+# 
+# 
+#   def name
+#     @name
+#   end
+# 
+# 
+# 
+# end
