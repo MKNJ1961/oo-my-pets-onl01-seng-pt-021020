478835178fcde22733e2e52cc1bb778dc1316573
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 18:09:10 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,10 @@
-# class Cat
-#   attr_accessor :name
-#
-#   def initialize(name)
-#     @name = name
-#   end
-#   def name
-#     @name
-#   end
-# end
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
