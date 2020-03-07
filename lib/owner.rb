5db885fc704c513a5e4030f11da4b2ce97cb7686
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 20:04:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
 
   attr_reader :species
-  attr_accessor :pets, :owner, :name
+ attr_accessor :mood, :name, :pets
   #Instance Methods
 
   @@all = []
@@ -16,8 +16,9 @@ class Owner
 
   def initialize(name, species="human")
     @name = name
+    @pets = {cats: [], dogs: []}
     @species = species
-    @all << self
+    @@all << self
   end
 
   def say_species
