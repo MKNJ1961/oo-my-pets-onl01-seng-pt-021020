f6c99a7b22a4f8a2eb719bbf8721f3cc71e3d2ce
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 18:08:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,17 @@
 class Owner
   attr_accessor :name, :pets
 
-
+  
   #Instance Methods
 
+  def initialize(name, pets)
+    @name = name
+    @pets = pets
+  end
+  
+  def name
+    @name
+  end
 
 
   # code goes here
