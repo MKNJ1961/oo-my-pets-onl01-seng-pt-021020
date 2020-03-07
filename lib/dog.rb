5408e0819bb6787713783135e396aa07b32d3c16
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 18:09:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
-# class Dog
-#   attr_accessor :name
-#
-#   def initialize(name)
-#     @name = name
-#   end
-#   def name
-#     @name
-#   end
-# end
+class Dog
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
