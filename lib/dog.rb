376f29a7e5dcf9b4cd3462895fb8e093958f942d
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-07 22:49:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,22 @@
 class Dog
+  attr_accessor :mood, :owner
   attr_reader :name
 
-  def initialize(name)
+  @@all = []
+
+  #Instance Methods
+
+  def initialize(name, owner)
     @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+    owner.dogs << self
   end
 
+  #Class Methods
+
+  def self.all
+    @@all
+  end
 end
