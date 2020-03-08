bb94449345a065f1ac8d6b56b3bd1c1697372d17
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-08 02:00:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,13 +46,7 @@ class Owner
     self.cats.each {|cat| cat.mood = "happy"}
   end
 
-  # def sell_pets
-  #   self.cats.each do |cat|
-  #     self.cats.delete(cat)
-  #     cat.owner = nil
-  #     cat.mood = "nervous"
-  #   end
-  # end
+  # 0
 
   def sell_pets
     self.dogs.each do |dog|
